91328dbcc75c41da861a8b499aaaa5b0fa5d6b4a
Aski0 <mlipior79@gmail.com>
2025-06-25 20:02:22 +0200

    dodanie adresu frontu jako zmienna środowiskowa

--- src/main/java/com/example/forecastapp/config/WebConfig.java
+++ src/main/java/com/example/forecastapp/config/WebConfig.java
@@ -1,16 +1,18 @@
 package com.example.forecastapp.config;
 
+import lombok.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Value("${VITE_API_BASE_URL}")
+    private String frontendUrl;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

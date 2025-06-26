8c71cfbba46daf5901aa19787b804975cfa84e45
Aski0 <mlipior79@gmail.com>
2025-06-26 10:09:52 +0200

    CORS update

--- src/main/java/com/example/forecastapp/config/WebConfig.java
+++ src/main/java/com/example/forecastapp/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.forecastapp.config;
 
+import lombok.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://forecastappfrontend.onrender.com")
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://forecastappfrontend.onrender.com","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
